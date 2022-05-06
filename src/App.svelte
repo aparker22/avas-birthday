8b52b2f58bcbc4a8f1b7ed962af4462f9ec34887
Ashley Parker <aparker@bignerdranch.com>
2022-05-06 10:17:32 -0400

    my first svelte commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	// export let name: string = "Ava";
+	import "./Invitation.svelte";
+	import Invitation from "./Invitation.svelte";
+	let src = "images/strange.jpeg";
+</script>
+
+<main>
+	<h3>Ava's Party Page</h3>
+	<!-- <p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p> -->
+	<img {src} alt="Dr. Strange" />
+	<Invitation />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+		background-color: black;
+		display: flex;
+		width: 100%;
+		flex-direction: column;
+	}
+
+	h3 {
+		color: #c43737;
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 400;
+		margin: 10px;
+	}
+
+	img {
+		width: 100%;
+	}
+
+	@media (min-width: 460px) {
+		h3 {
+			font-size: 3em;
+		}
+	}
+</style>
---
+++ src/Invitation.svelte
@@ -0,0 +1,66 @@
+<div>
+    <p class="invite">YOU ARE INVITED</p>
+    <p class="strange">to a private screening of Dr. Strange</p>
+    <h4>Sunday, May 15 at 1pm</h4>
+    <h4>Regal Hollywood</h4>
+    <h5>
+        <a
+            href="https://www.google.com/maps/place/Regal+Hollywood+@+North+I-85/@33.8709574,-84.2815007,17z/data=!3m1!4b1!4m5!3m4!1s0x88f508246fa72eb9:0x38c26bf8a6dde340!8m2!3d33.8709872!4d-84.2770246"
+            >Directions to the theater</a
+        >
+    </h5>
+    <h5>Tickets and concessions will be provided</h5>
+
+    <div class="border" />
+
+    <p class="drop-off">
+        Parents can drop off at Ava's house on Sunday morning. We will leave the
+        house at 12:15.
+    </p>
+    <h5>
+        <a
+            href="https://www.google.com/maps/place/610+Greystone+Park+NE,+Atlanta,+GA+30324/@33.8009103,-84.369496,17z/data=!3m1!4b1!4m5!3m4!1s0x88f50432df725be7:0x5b67d1f93a9142c9!8m2!3d33.8009059!4d-84.3673073"
+            ><h5>610 Greystone Park NE</h5>
+            <h5>Atlanta, GA 30324</h5></a
+        >
+    </h5>
+    <p>We will have refreshments at Ava's house after the movie.</p>
+    <h1>Please RSVP to (912) 614-2045</h1>
+    <h4>(Ava's Mom Ashley)</h4>
+</div>
+
+<style>
+    div {
+        margin: 20px;
+    }
+    p {
+        margin: 0;
+    }
+    .invite {
+        color: #eedfdf;
+        font-size: 20px;
+    }
+    .strange {
+        color: #eedfdf;
+        margin: 5px;
+    }
+    h4 {
+        color: #c43737;
+    }
+    h5 {
+        margin: 2px;
+    }
+
+    .drop-off {
+        color: white;
+        margin-top: 10px;
+    }
+
+    .border {
+        border: 2px dashed white;
+    }
+    h1 {
+        color: #c43737;
+        margin-bottom: 0;
+    }
+</style>
